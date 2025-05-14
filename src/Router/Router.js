0201e8aa4e83bded098602dc25ce79c3a9dd9c9d
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-15 01:52:38 +0600

    router

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from 'react-router'
 import Root from '../components/Root'
+import Home from '../components/Home'
+import Header from '../components/Header'
+import AddCoffee from '../components/AddCoffee'
+import UpdateCoffee from '../components/UpdateCoffee'
 
 export const router=createBrowserRouter([
     {
         path:"/",
-        Component:Root
+        Component:Root,
+        children:[
+            {
+                index:true, Component:Home
+            },
+
+            {
+                path:"header",
+                Component:Header
+            },
+            {
+                path:"addcoffee",
+                Component:AddCoffee
+            },
+            {
+                path:"updatecoffee",
+                Component:UpdateCoffee
+            }
+        ]
     }
 
 ])
