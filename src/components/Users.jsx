2741daba12c05a92acec1ef482989174eb3a6fb6
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-05-17 00:18:57 +0600

    delete

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,66 +1,94 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
-    const initailUsers=useLoaderData();
-    const [users,setUsers]=useState(initailUsers);
+  const initailUsers = useLoaderData();
+  const [users, setUsers] = useState(initailUsers);
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/profiles/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+                 const remaingUser=users.filter(user=>user._id !==id)
+                 setUsers(remaingUser)
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your profile has been deleted.",
+                icon: "success",
 
-    return (
-        <div>
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          NO.
-        </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      
-      {
-        users.map((user,index)=><tr key={user._id}> 
-        <th>
-          {index+1}
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle h-12 w-12">
-                <img
-                  src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                  alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div>
-          </div>
-        </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br />
-          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-        </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>)
+              });
+            }
+          });
       }
-     
-    </tbody>
-    
-  </table>
-</div>
-        </div>
-    );
+    });
+  };
+
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>NO.</th>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={user.photo}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user.name}</div>
+                      <div className="text-sm opacity-50">{user.address}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+                <th className="flex gap-2">
+                  <button className="btn p-4 btn-accent btn-xs">V</button>
+                  <button className="btn btn-info  btn-xs p-4">E</button>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-error btn-xs p-4"
+                  >
+                    D
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Users;
